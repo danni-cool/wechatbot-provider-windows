c4017600480ceea60a3bdd4b3a43872ce26dea43
danni.cool <contact@danni.cool>
2025-01-06 11:38:01 +0800

    fix: update Python installer URL and tag Docker image as latest for consistency

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,4 @@ docker build \
     --platform linux/amd64 \
     --progress plain \
     -f docker/Dockerfile \
-    -t dannicool/wechatbot-provider-windows:wc-3935 .
+    -t dannicool/wechatbot-provider-windows:latest .
